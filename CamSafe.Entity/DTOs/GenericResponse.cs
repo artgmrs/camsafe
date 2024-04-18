23075351746dff5e7d080413398f8c03672c996c
Arthur Guimarães <39536084+artgmrs@users.noreply.github.com>
2024-04-18 14:48:59 -0300

    fix: Removing unnused property

--- CamSafe.Entity/DTOs/GenericResponse.cs
+++ CamSafe.Entity/DTOs/GenericResponse.cs
@@ -3,14 +3,7 @@
     public class GenericResponse<T>
     {
         public T Results { get; set; }
-        public List<string> ErrorsMessages { get; set; }
-        public List<string> Messages { get; set; }
+        public List<string> ErrorsMessages { get; set; } = new List<string>();
         public int? StatusCode { get; set; }
-
-        public GenericResponse()
-        {
-            ErrorsMessages = new List<string>();
-            Messages = new List<string>();
-        }
     }
 }
